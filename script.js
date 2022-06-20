6b97442634acd00489f9315a6e8c528ef611dc8e
TarikMazloum <100148515+TarikMazloum@users.noreply.github.com>
2022-06-20 10:10:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+class MCLancheAlegre{
+    constructor(hamurguer, carne, queijo){
+        this.hamurguer = hamurguer;     
+        this.carne = carne;
+        this.queijo = queijo;
+    }
+
+    acompanhamentosMClancheAlegre(batatapequena, fruta){
+        if (this.MCLancheAlegre === MCLancheAlegre) {
+           
+        }
+    }
+
+}
+class BigLanche{
+    constructor(hamburgueres, alface, queijo, molhBacana, cebola, picles, pao){
+        this.hamburgueres = hamburgueres;     
+        this.alface = alface;
+        this.queijo = queijo;
+        this.molhBacana = molhBacana;
+        this.cebola = cebola;
+        this.picles = picles;
+        this.pao = pao
+    }
+
+    acompanhamentosBigLanche(batatamedia, Cocaglue){
+        if (this.BigLanche === BigLanche) {
+
+        }
+    }
+
+}
+class McHen{
+    constructor(queijo, hamburguerfrango, alface, picles){
+        this.queijo = queijo;     
+        this.hamburguerfrango = hamburguerfrango;
+        this.alface = alface;
+        this.picles = picles
+    }
+
+    acompanhamentosMcHen(batatamedia, Cocaglue){
+        if (this.McHen === McHen) {
+            
+        }
+    }
+
+}
+
